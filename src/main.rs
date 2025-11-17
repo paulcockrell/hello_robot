bca73701eb381398b32dccb0b88a356b1f4acff1
Paul Cockrell <paulcockrell@gmail.com>
2025-11-17 21:09:21 +0000

    Add ultrasound

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use rppal::gpio::Gpio;
+use rppal::gpio::{Gpio, InputPin, Level, OutputPin};
 use serde::Deserialize;
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 use tokio::io::AsyncReadExt;
 use tokio::net::UnixListener;
 use tokio::task;
@@ -11,7 +12,63 @@ struct RobotState {
     ldr_left_value: u8,
     ldr_middle_value: u8,
     ldr_right_value: u8,
+    ultrasound_distance: u16,
     last_cmd: String,
+}
+
+struct UltrasoundSensor {
+    trig: OutputPin,
+    echo: InputPin,
+    max_wait: Duration,
+    speed_of_sound: f32,
+}
+
+impl UltrasoundSensor {
+    pub fn new(trig_pin: u8, echo_pin: u8) -> Self {
+        let gpio = Gpio::new().unwrap();
+
+        Self {
+            trig: gpio.get(trig_pin).unwrap().into_output_low(),
+            echo: gpio.get(echo_pin).unwrap().into_input_pulldown(),
+            max_wait: Duration::from_micros(25_000), // 25ms = max sensor timeout
+            speed_of_sound: 343.0,                   // m/s @ ~20 deg c
+        }
+    }
+
+    fn measure_cm(&mut self) -> Option<u16> {
+        // Trigger pulse
+        self.trig.set_low();
+        std::thread::sleep(Duration::from_micros(2));
+
+        self.trig.set_high();
+        std::thread::sleep(Duration::from_micros(10));
+        self.trig.set_low();
+
+        // Wait for echo to go high
+        let start_wait = Instant::now();
+        while self.echo.read() == Level::Low {
+            if start_wait.elapsed() > self.max_wait {
+                return None; // no wait
+            }
+        }
+
+        // Echo High, start timing pulse
+        let pulse_start = Instant::now();
+        while self.echo.read() == Level::High {
+            if pulse_start.elapsed() > self.max_wait {
+                return None;
+            }
+        }
+
+        let pulse_duration = pulse_start.elapsed();
+
+        // Convert pulse time to distance
+        let secs = pulse_duration.as_secs_f32();
+
+        let distance_cm = (secs * self.speed_of_sound * 100.0) / 2.0;
+
+        return Some(distance_cm as u16);
+    }
 }
 
 fn ldr(state: Arc<Mutex<RobotState>>) {
@@ -69,6 +126,7 @@ async fn main() {
         ldr_left_value: 0,
         ldr_middle_value: 0,
         ldr_right_value: 0,
+        ultrasound_distance: 0,
         last_cmd: "none".into(),
     }));
 
@@ -93,7 +151,26 @@ async fn main() {
     }
 
     //
-    // Task C - Global robot logic loop
+    // Task C - Ultrasound sensor
+    //
+    {
+        let state = Arc::clone(&state);
+        let mut us_sensor = UltrasoundSensor::new(11, 8);
+
+        task::spawn_blocking(move || {
+            loop {
+                {
+                    let mut s = state.lock().unwrap();
+                    s.ultrasound_distance = us_sensor.measure_cm().unwrap_or(0);
+                }
+
+                std::thread::sleep(Duration::from_millis(60));
+            }
+        });
+    }
+
+    //
+    // Task D - Global robot logic loop
     //
     loop {
         let s = state.lock().unwrap();
